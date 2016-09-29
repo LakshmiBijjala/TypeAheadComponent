5e6b69fbc457b56b19feddcad7643bcce1d81658
Lakshmi Bijjala <LakshmiBijjala@users.noreply.github.com>
2016-09-29 13:10:06 +0530

    Create typeaheadctrl.js

---
+++ typeaheadctrl.js
@@ -0,0 +1,10 @@
+angular.module('app').controller('typeaheadctrl', ['$scope', function($scope) {
+
+  $scope.typeaheadData = [{ 'name': 'example1',
+                            'id': '1'
+                          },
+                          { 'name': 'example2',
+                            'id': '2'
+                          }];
+
+]);
